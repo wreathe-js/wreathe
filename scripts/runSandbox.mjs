1a5e6166bb67e2bce8225da45a1436ca8b464bc3
jrson83 <me@jrson.de>
2023-01-18 00:55:31 +0100

    chore(workspace): add script to run sandbox dev & artisan

---
+++ scripts/runSandbox.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+// @ts-check
+import { shell } from '@tunnckocore/execa'
+import minimist from 'minimist'
+import { resolve } from 'node:path'
+import { exists, notifications } from './utils/index.mjs'
+
+async function run() {
+  const __dirname = process.cwd()
+
+  const { ui } = minimist(process.argv.slice(2))
+
+  if (!ui) {
+    console.error('--ui flag is missing.')
+    process.exit(1)
+  }
+
+  const dirExists = await exists(resolve(__dirname, 'sandboxes', ui))
+
+  if (!dirExists) {
+    console.error(
+      `${notifications.fail} The sandbox directory ./sandboxes/${ui} does not exist.\n`
+    )
+    console.error(
+      `${notifications.info} To setup a new sandbox run: pnpm run sandbox\n`
+    )
+    process.exit(1)
+  }
+
+  await shell(['pnpm run dev', 'php artisan serve'], {
+    stdio: 'inherit',
+    cwd: resolve(__dirname, 'sandboxes', ui),
+  })
+}
+
+run()
