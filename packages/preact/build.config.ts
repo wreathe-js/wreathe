b0b9f5c5e919ca12676650b88913f96dfd887f5a
Jrson <45284565+jrson83@users.noreply.github.com>
2023-03-07 05:19:35 +0100

    fix(workspace): remove fix for esbuild minification

--- packages/preact/build.config.ts
+++ packages/preact/build.config.ts
@@ -10,15 +10,4 @@ export default defineBuildConfig({
       minify: true,
     },
   },
-  /* https://github.com/unjs/unbuild/issues/112#issuecomment-1331769536 */
-  hooks: {
-    'rollup:dts:options'(_ctx, options) {
-      if (Array.isArray(options.plugins)) {
-        const esbuildPlugin = options.plugins.find(
-          (p) => (p as any).name === 'esbuild'
-        )
-        ;(esbuildPlugin as any).renderChunk = () => null
-      }
-    },
-  },
 })
