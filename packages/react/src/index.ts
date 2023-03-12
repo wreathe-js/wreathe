8e21578908c14d6f53569ad5b6785264c77773d3
Jrson <45284565+jrson83@users.noreply.github.com>
2023-03-12 09:00:51 +0100

    fix(react): remove test types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -11,12 +11,3 @@ export { default as Link } from './Link'
 export { default as useForm } from './useForm'
 export { default as usePage } from './usePage'
 export { default as useRemember } from './useRemember'
-
-export interface DefaultWreatheComponentProps<
-  SharedProps extends PageProps = PageProps
-> {
-  props: Page<SharedProps>['props']
-}
-
-export type WreathePageProps<SharedProps extends PageProps = PageProps> =
-  Page<SharedProps>['props']
