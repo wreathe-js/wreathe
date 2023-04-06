b5efa33b06e82b1575b3a1c7a7db0831552aaada
Jrson <45284565+jrson83@users.noreply.github.com>
2023-04-06 03:23:33 +0200

    fix(preact): index exports

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,7 @@
-import { type Page, type PageProps, router as Router } from '@wreathe-js/core'
+import { router as Router } from '@wreathe-js/core'
 
-export type { Page, PageProps, Errors, ErrorBag } from '@wreathe-js/core'
 export const router: typeof Router = Router
-export {
-  default as createWreatheApp,
-  type ReactComponent,
-} from './createWreatheApp'
+export { default as createWreatheApp } from './createWreatheApp'
 export { default as Head } from './Head'
 export { default as Link } from './Link'
 export { default as useForm } from './useForm'
